2d61b93b5bc173f27909d97b096eaa1dc4d82809
Bruno Vargas <brunocalegaridevargas@gmail.com>
2023-05-09 15:34:22 -0300

    added join()

---
+++ Metodo_join4/src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+
+        MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 500);
+        MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 500);
+        MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 500);
+
+        Thread t1 = new Thread(thread1);
+        Thread t2 = new Thread(thread2);
+        Thread t3 = new Thread(thread3);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Programa finalizado.");
+    }
+}
