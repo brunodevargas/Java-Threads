ba91a1bbd4018435b52c65daa9a2abd562ab9c3d
Bruno Vargas <brunocalegaridevargas@gmail.com>
2023-05-09 14:40:51 -0300

    added Thread3

---
+++ Thread3_Runnable/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+
+        MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 500);
+        MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 1000);
+        MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 300);
+
+    }
+}
